478b2f642e9006e6129708a9aa335c03765d7bfd
Dinar <adejke@gmail.com>
2026-09-13 20:05:49 +0300

    feat: add collection detail header and custom navigation

---
+++ FakeNFT/Scenes /Catalog/Views/CollectionDetailHeaderView.swift
@@ -0,0 +1,128 @@
+//
+//  CollectionDetailHeaderView.swift
+//  FakeNFT
+//
+//  Created by Dinar Mukhlisov on 12.09.2026.
+//
+
+import UIKit
+
+final class CollectionDetailHeaderView: UICollectionReusableView {
+    
+    lazy var contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(authorContainer)
+        stackView.addArrangedSubview(descriptionLabel)
+        
+        stackView.setCustomSpacing(8, after: titleLabel)
+        stackView.setCustomSpacing(0, after: authorContainer)
+        
+        return stackView
+    }()
+    
+    lazy var authorButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitleColor(UIColor(resource: .ypBlueUniversal), for: .normal)
+        button.titleLabel?.font = .caption1
+        button.setContentHuggingPriority(.required, for: .horizontal)
+        return button
+    }()
+    
+    lazy var authorContainer: UIStackView = {
+        let container = UIStackView()
+        container.spacing = 4
+        container.alignment = .center
+        container.distribution = .fill
+        
+        let authorTitleLabel = UILabel()
+        authorTitleLabel.textColor = UIColor(resource: .ypBlack)
+        authorTitleLabel.font = .caption2
+        authorTitleLabel.text = NSLocalizedString("Collection.author.title", comment: "")
+        authorTitleLabel.setContentHuggingPriority(.required, for: .horizontal)
+        authorTitleLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        let spacer = UIView()
+        spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        
+        container.addArrangedSubview(authorTitleLabel)
+        container.addArrangedSubview(authorButton)
+        container.addArrangedSubview(spacer)
+        
+        return container
+    }()
+    
+    let coverImageView: UIImageView = {
+        let coverImageView = UIImageView()
+        coverImageView.translatesAutoresizingMaskIntoConstraints = false
+        coverImageView.contentMode = .scaleAspectFill
+        coverImageView.clipsToBounds = true
+        
+        coverImageView.layer.cornerRadius = 12
+        coverImageView.layer.maskedCorners = [
+            .layerMinXMaxYCorner,
+            .layerMaxXMaxYCorner
+        ]
+        return coverImageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(resource: .ypBlack)
+        label.font = .headline3
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(resource: .ypBlack)
+        label.font = .caption2
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let coverImageHeight: CGFloat = 310
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayoutAndConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        nil
+    }
+    
+    func configure(with collection: NFTCollection) {
+        if let url = URL(string: collection.cover) {
+            coverImageView.kf.setImage(with: url)
+        }
+        titleLabel.text = collection.name
+        descriptionLabel.text = collection.description
+        authorButton.setTitle(collection.author, for: .normal)
+        
+    }
+    
+    private func setupLayoutAndConstraints() {
+        addSubview(coverImageView)
+        addSubview(contentStackView)
+        
+        NSLayoutConstraint.activate([
+            coverImageView.topAnchor.constraint(equalTo: topAnchor),
+            coverImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            coverImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            coverImageView.heightAnchor.constraint(equalToConstant: coverImageHeight),
+            
+            contentStackView.topAnchor.constraint(equalTo: coverImageView.bottomAnchor, constant: 16),
+            contentStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            contentStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            contentStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
+
--- FakeNFT/Scenes /Catalog/Views/CollectionDetailViewController.swift
+++ FakeNFT/Scenes /Catalog/Views/CollectionDetailViewController.swift
@@ -11,6 +11,24 @@ final class CollectionDetailViewController: UIViewController {
     
     private let collection: NFTCollection
     
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        collectionView.contentInsetAdjustmentBehavior = .never
+        return collectionView
+    }()
+    
+    private lazy var navBarBackCustomButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        button.tintColor = UIColor(resource: .ypBlack)
+        button.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
+        return button
+    }()
+    
     init(collection: NFTCollection) {
         self.collection = collection
         super.init(nibName: nil, bundle: nil)
@@ -19,10 +37,107 @@ final class CollectionDetailViewController: UIViewController {
     @available(*, unavailable)
     required init?(coder: NSCoder) { nil }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBackground
-        title = collection.name
+        view.backgroundColor = UIColor(resource: .ypWhite)
+        setupCollectionView()
+        setupLayoutAndConstraints()
+        
+        print(collection)
+    }
+    
+    @objc
+    private func didTapBackButton() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(CollectionDetailHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
+        
+        collectionView.alwaysBounceVertical = true
+    }
+    
+    private func setupLayoutAndConstraints() {
+        view.addSubview(collectionView)
+        view.addSubview(navBarBackCustomButton)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            navBarBackCustomButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            navBarBackCustomButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+        ])
+    }
+}
+
+extension CollectionDetailViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int
+    ) -> CGSize {
+
+        let width = collectionView.bounds.width
+
+        let header = CollectionDetailHeaderView(
+            frame: CGRect(x: 0, y: 0, width: width, height: 0)
+        )
+
+        header.configure(with: collection)
+
+        let targetSize = CGSize(
+            width: width,
+            height: UIView.layoutFittingCompressedSize.height
+        )
+
+        let size = header.systemLayoutSizeFitting(
+            targetSize,
+            withHorizontalFittingPriority: .required,
+            verticalFittingPriority: .fittingSizeLevel
+        )
+
+        return CGSize(
+            width: width,
+            height: ceil(size.height)
+        )
+    }
+}
+
+extension CollectionDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: "header",
+            for: indexPath
+        ) as? CollectionDetailHeaderView else {
+            return UICollectionReusableView()
+        }
+        
+        header.configure(with: collection)
+        return header
     }
 }
