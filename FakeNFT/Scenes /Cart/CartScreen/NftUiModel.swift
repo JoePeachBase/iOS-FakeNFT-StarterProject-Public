59fef5ac1b86072d36ea488a74396367bcc1cb0d
Мамытов Руслан <rmamytov@sberbank.ru>
2026-09-15 20:05:16 +0700

    feat: Добавил UI модели для Nft, Order и Currency

---
+++ FakeNFT/Scenes /Cart/CartScreen/NftUiModel.swift
@@ -0,0 +1,14 @@
+//
+//  NftUiModel.swift
+//  FakeNFT
+//
+//  Created by Мамытов Руслан on 15.09.2026.
+//
+import Foundation
+
+struct NftUiModel {
+    let title: String
+    let imageURL: URL
+    let rating: Int
+    let price: String
+}
